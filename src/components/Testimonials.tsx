1646dc66eeaa3bec2e10c4daf7d05ce6ad8b76e4
Hesham El Mahdi <heshamelmahdi@gmail.com>
2024-07-10 15:47:06 +0300

    testimonials modification

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -5,19 +5,19 @@ export const testimonials = [
   {
     quote:
       "Working with Hesham on phieg.com was an absolute pleasure. Their expertise in creating a sleek and animated landing page truly brought our vision to life. The design not only captivated our audience but also enhanced user engagement. Highly recommend!",
-    name: "Ahmed El-Mahdi",
+    name: "Ahmed",
     title: "Founder and CEO",
   },
   {
     quote:
       "We are thrilled with the e-commerce platform developed by Hesham for cairo3dprint.com. Their solution seamlessly integrates file uploads and printing services, making it incredibly user-friendly. The website has boosted our operational efficiency and customer satisfaction. Exceptional work!",
-    name: "Hesham Mohamed",
+    name: "Iman",
     title: "Founder and CEO",
   },
   {
     quote:
       "Implementing phiegcrm.com with Hesham was transformative for our lead management. Their integration of Facebook and TikTok functionalities streamlined lead distribution and updates, significantly enhancing our client interactions. A game-changer for our business!",
-    name: "Mostafa Ahmed",
+    name: "Mostafa",
     title: "General Manager",
   },
 ];
